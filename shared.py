812ee91b212ec9db6b3813c31bdbbf732063fde7
Predator04 <predator04@users.noreply.github.com>
2026-06-22 06:15:26 -0700

    v7.5: Bump version to 7.5 (build 2026-06-22)

--- shared.py
+++ shared.py
@@ -10,8 +10,8 @@ import ctypes
 from flask import Response, jsonify, request
 
 COAGENT_DIR = Path(__file__).parent.resolve()
-VERSION = "7.4"
-BUILD = "2026-06-19"
+VERSION = "7.5"
+BUILD = "2026-06-22"
 AGENT_NAME = "Hermes CoAgent"
 MACROS_DIR = COAGENT_DIR / "macros"
 SCREENSHOTS_DIR = COAGENT_DIR / "screenshots"
